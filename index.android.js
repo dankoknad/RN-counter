cfb698256c7639897bebab15ca426f1eb47e0dd8
dankoknad <dankomilutinovic@gmail.com>
2017-05-13 10:42:13 +0200

    first commit

---
+++ index.android.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+  AppRegistry,  
+	View,
+} from 'react-native';
+
+import Home from './src/components/Home'
+
+export default class RNCounter extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('RNCounter', () => RNCounter);
