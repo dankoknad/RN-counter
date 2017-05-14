40b25b9c4127466c04101c4aea1efe0962a2bf3a
dankoknad <dankomilutinovic@gmail.com>
2017-05-14 10:25:52 +0200

    settings page improvements

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -8,7 +8,9 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+	TouchableHighlight,
+	Alert,
 } from 'react-native';
 
 export default class Info extends Component {
@@ -21,6 +23,44 @@ export default class Info extends Component {
         <Text style={styles.welcome}>
           On this page user will be able to {'\n'} lock/unlock slider and disable/enable buttons.
         </Text>
+				<Text style={[styles.instructions, styles.bigText]}>
+          Slider settings
+        </Text>
+				<Text style={[styles.fullWidth]}>
+          Min value: 1
+        </Text>
+				<Text style={[styles.fullWidth]}>
+          Max value: 10
+        </Text>
+				<Text style={[styles.fullWidth]}>
+          Steps: 1
+        </Text>
+
+				<Text style={[styles.instructions, styles.bigText]}>
+          Enable/disable buttons
+        </Text>
+				<View style={styles.btnsContainer}>
+					<TouchableHighlight
+						onPress={() => Alert.alert(
+            'Alert 01 Title',
+            'alertMessage',
+            [{text: 'Dismiss alert 01'}],
+          )}
+						style={styles.button}
+					>
+						<Text style={styles.btnText}>btn 01</Text>
+					</TouchableHighlight>
+					<TouchableHighlight 
+						onPress={() => Alert.alert(
+            'Alert 02 Title',
+            'alertMessage',
+            [{text: 'Dismiss alert 02'}],
+          )}
+						style={styles.button}
+					>
+						<Text style={styles.btnText}>btn 02</Text>
+					</TouchableHighlight>
+				</View>
       </View>
     );
   }
@@ -45,6 +85,25 @@ const styles = StyleSheet.create({
   },
 	bigText: {
 		fontSize: 30,
+	},
+	fullWidth: {
+		alignSelf: 'stretch',
+		paddingHorizontal: 20,
+	},
+	btnsContainer: {
+		alignSelf: 'stretch',
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	button: {
+		marginHorizontal: 10,
+		borderRadius: 3,
+	},
+	btnText: {
+		padding: 10,
+		fontSize: 18,
+		color: '#fff',
+		backgroundColor: '#9a67ea'
 	}
 });
 
